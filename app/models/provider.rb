bd525ddedf3c2533097aaf533bf999142493ec56
shizuki-okuma <126657775+shizuki-okuma@users.noreply.github.com>
2023-05-10 07:42:17 +0000

    [add start]controller admin#home

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,5 @@
 class Provider < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
