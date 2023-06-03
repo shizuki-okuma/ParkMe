539e31d888a5fef8d0a81ccde33b0f49613377f1
shizuki-okuma <126657775+shizuki-okuma@users.noreply.github.com>
2023-06-03 23:59:59 +0900

    [add]validates zipcode

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -6,7 +6,7 @@ class Provider < ApplicationRecord
   validates :last_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :zip_code, presence: true
+  validates :zip_code, presence: true, length: { is: 7 }
   validates :address, presence: true
   validates :phone_number, presence: true
   validates :email, presence: true
